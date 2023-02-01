3b4c973bc9810dd33f8133736639134d2ce52fe2
sidrathore <rathoresiddharth0207@gmail.com>
2023-02-01 11:19:41 +0530

    welcome page naviagtion

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,18 +1,32 @@
-import 'package:budget_men/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
+  @override
+  State<WelcomeScreen> createState() {
+    return Welcome();
+  }
+}
+
+class Welcome extends State<WelcomeScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () => Navigator.pushNamed(context, '/sign_up'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Container(
-            child: InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/sign_up');
-                },
-                child: Text('Welcome to Ruprope',style: TextStyle(fontSize: 40,color: Colors.red,fontStyle: FontStyle.normal),)),
+            child: Text(
+              'welcome to budgetman',
+              style: TextStyle(fontSize: 30, color: Colors.red),
+            ),
           ),
         ),
       ),
